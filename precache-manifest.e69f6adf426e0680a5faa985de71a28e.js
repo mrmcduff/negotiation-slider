53f081c7931be3a79bfbe6549de699e257288c16
Michael McDuffee <mrmcduff@gmail.com>
2019-11-18 17:51:25 -0800

    Updates

---
+++ precache-manifest.e69f6adf426e0680a5faa985de71a28e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d556cea2fc60661181b519379e3cb838",
+    "url": "/negotiation-slider/index.html"
+  },
+  {
+    "revision": "ea459fd5546280b06768",
+    "url": "/negotiation-slider/static/css/main.f05016c4.chunk.css"
+  },
+  {
+    "revision": "9c26f88132659a33ec80",
+    "url": "/negotiation-slider/static/js/2.1cbec5e2.chunk.js"
+  },
+  {
+    "revision": "ea459fd5546280b06768",
+    "url": "/negotiation-slider/static/js/main.26fbee61.chunk.js"
+  },
+  {
+    "revision": "c21b6633946590a9b117",
+    "url": "/negotiation-slider/static/js/runtime-main.c57c4144.js"
+  }
+]);
